aa136ddc85c701b1f22bd31905e797115efd2c98
alejandro <levinasale@gmail.com>
2017-06-11 17:02:07 -0300

    d7

--- hello/views.py
+++ hello/views.py
@@ -12,7 +12,7 @@ from .models import Greeting
     
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    return render(request, 'index2.html')
 
 
 def db(request):
